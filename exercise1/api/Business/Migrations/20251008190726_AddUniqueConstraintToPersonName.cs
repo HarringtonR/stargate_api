899f8a798e392fe76fdedb40b258e3706a553ff7
Ross <ross.ludeke@gmail.com>
2025-10-08 15:57:06 -0500

    unique name constraint

---
+++ exercise1/api/Business/Migrations/20251008190726_AddUniqueConstraintToPersonName.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StargateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToPersonName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_Name",
+                table: "Person",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Person_Name",
+                table: "Person");
+        }
+    }
+}
