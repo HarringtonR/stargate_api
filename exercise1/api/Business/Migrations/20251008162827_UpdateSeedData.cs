8fee4a9694fa65e7189b2275000bf16cb9c4edf0
Ross <ross.ludeke@gmail.com>
2025-10-08 13:43:37 -0500

    Initial commit

---
+++ exercise1/api/Business/Migrations/20251008162827_UpdateSeedData.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StargateAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CareerStartDate",
+                table: "AstronautDetail",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Person",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "John Doe" },
+                    { 2, "Jane Doe" },
+                    { 3, "Samantha Carter" },
+                    { 4, "Daniel Jackson" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AstronautDetail",
+                columns: new[] { "Id", "CareerEndDate", "CareerStartDate", "CurrentDutyTitle", "CurrentRank", "PersonId" },
+                values: new object[,]
+                {
+                    { 1, null, new DateTime(2010, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Commander", "1LT", 1 },
+                    { 2, null, new DateTime(2012, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Science Officer", "Major", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AstronautDuty",
+                columns: new[] { "Id", "DutyEndDate", "DutyStartDate", "DutyTitle", "PersonId", "Rank" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2015, 12, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2010, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Commander", 1, "1LT" },
+                    { 2, null, new DateTime(2016, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Mission Lead", 1, "Captain" },
+                    { 3, null, new DateTime(2012, 5, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Science Officer", 3, "Major" },
+                    { 4, null, new DateTime(2018, 3, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Pilot", 2, "Lieutenant" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AstronautDetail",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AstronautDetail",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AstronautDuty",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AstronautDuty",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "AstronautDuty",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "AstronautDuty",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Person",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CareerStartDate",
+                table: "AstronautDetail",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+        }
+    }
+}
